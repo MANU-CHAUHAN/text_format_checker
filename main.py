3a108d22367793a1d21e75d596a13d8f9e04a81a
Manu Chauhan <chamanu@sutherlandglobal.com>
2018-09-17 22:51:11 +0530

    changes

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ pattern_web_url = re.compile(
 
 pattern_email = re.compile(r'([a-zA-Z]+[a-zA-Z0-9_.]*@[a-zA-Z._]+?\.(?:com|net|org|edu|gov))')
 
+ignore_words = ['iPad', 'iPhone']
 
-def get_email_url_indices(txt):
+
+def get_email_url_ignore_word_indices(txt):
     span_set = set()
 
     r = pattern_email.finditer(txt)
@@ -20,8 +22,14 @@ def get_email_url_indices(txt):
     if r:
         for item in r:
             span_set.add(item.span())
-
     nums = set()
+    r = [re.finditer(r'\b{}\b'.format(w.lower()), txt.lower()) for w in ignore_words]
+
+    for i in r:
+        for j in i:
+            s, e = j.span()
+            if e - s > 0:
+                span_set.add((s, e))
     for start, end in span_set:
         if txt[start - 1] == ' ':
             nums.add((start - 1, end))
@@ -30,131 +38,153 @@ def get_email_url_indices(txt):
     return {i for s, e in nums for i in range(s, e)}
 
 
-def check_text_format(text):
-    text = text.strip()
-    mistakes = {}
-    count = 0
+end_states = set('.?!')
+capital_letters = set(string.ascii_uppercase)
+small_letters = set(string.ascii_lowercase)
+punctuations = set(string.punctuation)
+digits = set(string.digits)
+[punctuations.remove(x) for x in ['.', '?', '!']]
 
-    # check if the text starts and contains only email/url
-    email = pattern_email.match(text)
-    url = pattern_web_url.match(text)
-    if email:
-        s, e = email.span()
-        if e - s in [len(text), len(text) - 1]:
-            return 0, {}
-    if url:
-        s, e = url.span()
-        if e - s in [len(text), len(text) - 1]:
-            return 0, {}
 
-    first = text[0]
-    last = text[-1]
+def check_text_format(texts):
+    final_data = []
+    over_all_count = 0
+    flag = False
+    for text in texts:
+        if text:
+            # text = text.strip()
+            mistakes = {}
+            count = 0
+            # check if the text starts and contains only email/url
+            email = pattern_email.match(text)
+            url = pattern_web_url.match(text)
+            if email:
+                s, e = email.span()
+                if e - s in [len(text), len(text) - 1]:
+                    flag = True
+            if url:
+                s, e = url.span()
+                if e - s in [len(text), len(text) - 1]:
+                    flag = True
+            if not flag:
+                first = text[0]
+                last = text[-1]
 
-    if (first < 'A' or first > 'Z') and (first < '0' or first > '9'):
-        mistakes['start_letter_not_capital'] = True
-        count += 1
-    if last not in ['.', '?', '!']:
-        mistakes['end_not_proper'] = True
-        count += 1
+                if (first < 'A' or first > 'Z') and (first < '0' or first > '9'):
+                    mistakes['start_not_proper'] = True
+                    count += 1
+                if last not in ['.', '?', '!']:
+                    mistakes['end_not_proper'] = True
+                    count += 1
 
-    mistakes['extra_spaces'] = []
-    mistakes['space_used_before_.?!'] = []
-    mistakes['no_space_after_.?!'] = []
-    mistakes['capital_letter_missed_after_.?!_space'] = []
-    mistakes['capital_letter_missed_after_.?!'] = []
-    mistakes['capital_case_after_small_case'] = []
+                mistakes['extra_spaces'] = []
+                mistakes['space_used_before_.?!'] = []
+                mistakes['no_space_after_.?!'] = []
+                mistakes['capital_letter_missed_after_.?!_space'] = []
+                mistakes['capital_letter_missed_after_.?!'] = []
+                mistakes['capital_case_after_small_case'] = []
 
-    chars_dict = collections.defaultdict(int)
-    previous_to_previous_state = previous_state = current_state = 0
-    capital_letters = set(string.ascii_uppercase)
-    small_letters_digits_special_chars_set = set(string.ascii_lowercase + string.digits + string.punctuation)
-    [small_letters_digits_special_chars_set.remove(x) for x in ['.', '?', '!']]
+                chars_dict = collections.defaultdict(int)
+                previous_to_previous_state = previous_state = current_state = 0
 
-    index = 1
-    chars_dict[text[0]] += 1
-    length = len(text)
+                index = 1
+                chars_dict[text[0]] += 1
+                length = len(text)
+                indices_to_ignore = get_email_url_ignore_word_indices(text)
 
-    indices_to_ignore = get_email_url_indices(text)
+                while index < length:
+                    if index in indices_to_ignore:
+                        index += 1
+                        continue
+                    char = text[index]
+                    chars_dict[char] += 1
 
-    while index < length:
-        if index in indices_to_ignore:
-            index += 1
-            continue
-        char = text[index]
-        chars_dict[char] += 1
+                    if char in capital_letters:
+                        current_state = 0
 
-        if char in capital_letters:
-            current_state = 0
+                    if char == ' ':
+                        current_state = 1
 
-        if char == ' ':
-            current_state = 1
+                    if char in small_letters:
+                        current_state = 2
 
-        if char in small_letters_digits_special_chars_set:
-            current_state = 2
+                    if char in end_states:
+                        current_state = 3
 
-        if char in ['.', '?', '!']:
-            current_state = 3
+                    if char in digits:
+                        current_state = 4
 
-        '''checks for states'''
+                    if char in punctuations:
+                        current_state = 5
 
-        # check if space before '.'
-        if current_state == 3 and previous_state == 1:
-            mistakes['space_used_before_.?!'].append(index - 1)
-            count += 1
+                    '''checks for states'''
 
-        # check if space after space -> extra space used
-        elif previous_state == current_state == 1:
-            mistakes['extra_spaces'].append(index)
-            count += 1
+                    # check if space before '.'
+                    if current_state == 3 and previous_state == 1:
+                        mistakes['space_used_before_.?!'].append(index - 1)
+                        count += 1
 
-        # wrong case usage: abC, 'C' is capital case after small case.
-        elif current_state == 0 and previous_state == 2:
-            mistakes['capital_case_after_small_case'].append(index)
-            count += 1
+                    # check if space after space -> extra space used
+                    elif previous_state == current_state == 1:
+                        mistakes['extra_spaces'].append(index)
+                        count += 1
 
-        # ...blah. abc -> small case after '.' and space
-        if previous_to_previous_state == 3 and previous_state == 1 and current_state == 2:
-            mistakes['capital_letter_missed_after_.?!_space'].append(index)
-            count += 1
+                    # wrong case usage: abC, 'C' is capital case after small case.
+                    elif current_state == 0 and previous_state == 2:
+                        mistakes['capital_case_after_small_case'].append(index)
+                        count += 1
 
-        # no space after '.'
-        if current_state in [0, 2] and previous_state == 3:
-            mistakes['no_space_after_.?!'].append(index)
-            count += 1
+                    # ...blah. abc -> small case after '.' and space
+                    if previous_to_previous_state == 3 and previous_state == 1 and current_state == 2:
+                        mistakes['capital_letter_missed_after_.?!_space'].append(index)
+                        count += 1
 
-        # small letter after .
-        if current_state == 2 and previous_state == 3:
-            mistakes['capital_letter_missed_after_.?!'].append(index)
-            count += 1
+                    # no space after '.'
+                    if current_state in [0, 2] and previous_state == 3:
+                        mistakes['no_space_after_.?!'].append(index)
+                        count += 1
 
-        index += 1
-        previous_to_previous_state = previous_state
-        previous_state = current_state
+                    # small letter after .
+                    if current_state == 2 and previous_state == 3:
+                        mistakes['capital_letter_missed_after_.?!'].append(index)
+                        count += 1
 
-    # while ends
+                    index += 1
+                    previous_to_previous_state = previous_state
+                    previous_state = current_state
 
-    ''' check the number of pairs for '(' & ')', '{' & '}', '[' & ']', '<' & '>'   '''
-    if chars_dict['('] != chars_dict[')']:
-        mistakes['(_)_mismatch'] = True
-        count += 1
+                # while ends
 
-    if chars_dict['{'] != chars_dict['}']:
-        mistakes['{_}_mismatch'] = True
-        count += 1
+                ''' check the number of pairs for '(' & ')', '{' & '}', '[' & ']', '<' & '>'   '''
+                if chars_dict['('] != chars_dict[')']:
+                    mistakes['(_)_mismatch'] = True
+                    count += 1
 
-    if chars_dict['['] != chars_dict[']']:
-        mistakes['[_]_mismatch'] = True
-        count += 1
+                if chars_dict['{'] != chars_dict['}']:
+                    mistakes['{_}_mismatch'] = True
+                    count += 1
 
-    if chars_dict['<'] != chars_dict['>']:
-        mistakes['<_>_mismatch'] = True
-        count += 1
+                if chars_dict['['] != chars_dict[']']:
+                    mistakes['[_]_mismatch'] = True
+                    count += 1
 
-    return count, mistakes
+                if chars_dict['<'] != chars_dict['>']:
+                    mistakes['<_>_mismatch'] = True
+                    count += 1
+
+                if count > 0:
+                    over_all_count += count
+                    final_data.append(
+                        {'count': count,
+                         'mistakes': {x: y for x, y in mistakes.items() if y},
+                         'text': text})
+
+    return final_data, over_all_count
 
 
 if __name__ == '__main__':
-    s = "hello .hoW are you www.xyz.com? i hope all  good"
-    c, m = check_text_format(s)
-    print(s, '\n', c)
-    [print(x) for x in m.items()]
+    s1 = "hello .hoW are you www.xyz.com? i hope all  good"
+    s2 = "hey, where is  your iPhone"
+    data, total_mistakes = check_text_format([s1, s2])
+    print('\nTotal mistakes: ', total_mistakes, '\n')
+    [print('Text number: {0}    Info dict: {1}'.format(i, x)) for i, x in enumerate(data)]
